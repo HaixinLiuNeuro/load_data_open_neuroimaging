d39fcf29e8ed271acfdca2bf95b41b00150a4734
Haixin <46263698+HaixinLiuNeuro@users.noreply.github.com>
2025-02-19 21:39:49 -0600

    Update openneuro_dataset.py
    setup

--- datasets/openneuro_dataset.py
+++ datasets/openneuro_dataset.py
@@ -1,1 +1,25 @@
 
+import openneuro
+import os
+
+class OpenNeuroDataSet:
+  def __init__(self, dataset_id, target_dir='data'):
+    """
+    Initialize the dataset ID and target directory.
+    """
+    
+    self.dataset_id = dataset_id
+    self.target_dir = target_dir
+
+  def download(self):
+    """
+    Download the specified dataset to the target directory.
+    """
+
+    try:
+
+    except Exception as e:
+      print(f"An error occured while downloading the dataset: {e}")
+      
+  
+
